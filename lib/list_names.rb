ece85be92026a2e3ecba279953426c99f210b616
Eric Hale <eric_hale@comcast.net>
2021-09-02 15:24:32 -0600

    2nd solution

--- lib/list_names.rb
+++ lib/list_names.rb
@@ -1,17 +1,8 @@
 class ListNames
-    def list(hash_array)
-        list = ""
-        hash_array.each do |name|
-            if name[:name] == hash_array.last[:name] && hash_array.size > 1
-                list << "& " + name[:name]
-            elsif hash_array[-2] && name[:name] == hash_array[-2][:name]
-                list << name[:name] + " "
-            elsif hash_array.size == 1
-                list << name[:name]
-            else
-                list << name[:name] + ", " 
-            end
-        end
-        list
+    def list(names)
+        names = names.map { |name| name[:name] }
+        last_name = names.pop
+        return last_name.to_s if names.empty?
+        "#{names.join(', ')} & #{last_name}"
     end
 end
